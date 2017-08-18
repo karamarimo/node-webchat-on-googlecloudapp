262a577c4f5a3106ff89e4d94496b5305ec3c55a
karamarimo <kubotaryou.ryoukubota@gmail.com>
2017-08-18 18:40:04 +0900

    remove http(express) part from websockets server

--- src/server/websockets.js
+++ src/server/websockets.js
@@ -1,29 +1,10 @@
 'use strict';
 
-const express = require('express')
-const http = require('http')
 const socket = require('socket.io')
 const config = require('./config')
 const sql = require('./sql-model')
 
-const app = express()
-const server = http.Server(app)
-const io = socket(server)
-
-// ??? DO I NEED THIS ???
-// Basic 404 handler
-app.use((req, res) => {
-  res.status(404).send('Not Found')
-})
-
-// Basic error handler
-app.use((err, req, res, next) => {
-  /* jshint unused:false */
-  console.error(err)
-  // If our routes specified a specific response, then send that. Otherwise,
-  // send a generic message so as not to leak anything.
-  res.status(500).send(err.response || 'Something broke!')
-})
+const io = socket()
 
 // web socket handler
 io.on('connection', (socket) => {
@@ -77,10 +58,9 @@ io.on('connection', (socket) => {
 
 if (module === require.main) {
   // Start the server
-  server.listen(config.get('PORT_WEBSOCKETS'), () => {
-    const port = server.address().port
-    console.log(`App listening on port ${port}`)
-  })
+  const port = config.get('PORT_WEBSOCKETS')
+  io.listen(port)
+  console.log(`socket.io listening on port ${port}`)
 }
 
-module.exports = app
+module.exports = io
